49dda4c6303e0a003cb015f6c4d135331278a685
kumastry <chma19063@g.nihon-u.ac.jp>
2023-05-01 08:04:34 +0900

    strongparameterとリダイレクトの追加

--- app/controllers/postusers_controller.rb
+++ app/controllers/postusers_controller.rb
@@ -1,5 +1,15 @@
 class PostusersController < ApplicationController
   def create
-    User.create(name:params[:name], age:params[:age], intro:params[:intro])
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to '/users'
+    else
+      render :new
+    end
   end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :age, :intro)
+   end
 end
